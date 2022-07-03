310e471847b6e78fc3335b84952f4a62ce1fc04c
Jatin <jt24072002@gmail.com>
2022-07-04 00:44:26 +0530

    repeating and missing element done

---
+++ repeating_missing_element.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	vector<int> count(n, 0);
+    pair<int, int> ans;
+    for(int i=0; i<n; i++)
+        count[arr[i]-1]++;
+    for(int i=0; i<n; i++){
+        if(count[i]==0)
+            ans.first = i+1;
+        if(count[i]>1)
+            ans.second = i+1;
+    }
+    return ans;
+}

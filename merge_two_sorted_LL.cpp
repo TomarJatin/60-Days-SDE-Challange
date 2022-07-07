93be222d5ff1bae4228b08c28a2d4da4e4d82062
Jatin <jt24072002@gmail.com>
2022-07-07 23:01:56 +0530

    merge two sorted linked list using a dummy node done

---
+++ merge_two_sorted_LL.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    // Write your code here.
+    Node<int>* head = new Node<int>(-1);
+    Node<int>* temp = head;
+    while(first && second){
+        if(first->data < second->data){
+            temp->next = first;
+            temp = first;
+            first = first->next;
+        }
+        else{
+            temp->next = second;
+            temp = second;
+            second = second->next;
+        }
+    }
+    if(first)
+        temp->next = first;
+    if(second)
+        temp->next = second;
+    return head->next;
+}

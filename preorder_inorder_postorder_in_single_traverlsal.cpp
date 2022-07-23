805ab8c2a20e8184b6dbdf44c28cc32b2324c30a
Jatin <jt24072002@gmail.com>
2022-07-23 10:45:34 +0530

    all traversal in one traversal done

---
+++ preorder_inorder_postorder_in_single_traverlsal.cpp
@@ -0,0 +1,35 @@
+/************************************************************
+
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+************************************************************/
+void solve(BinaryTreeNode<int> *root, vector<vector<int>> &ans){
+    if(root == NULL)
+        return;
+    ans[1].push_back(root->data);
+    solve(root->left, ans);
+    ans[0].push_back(root->data);
+    solve(root->right, ans);
+    ans[2].push_back(root->data);
+}
+
+vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root){
+    vector<vector<int>> ans(3);
+    solve(root, ans);
+    return ans;
+}

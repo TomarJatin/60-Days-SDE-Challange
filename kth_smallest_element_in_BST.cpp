a3e4005bcb650842d5d773ddf394f9702d415542
Jatin <jt24072002@gmail.com>
2022-07-25 19:46:02 +0530

    kth smallest element done

---
+++ kth_smallest_element_in_BST.cpp
@@ -0,0 +1,31 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+int kthSmallest(TreeNode<int> *root, int& k)
+{
+	if(root == NULL) return -1;
+    int right = kthSmallest(root->left, k);
+    if(right!=-1)
+        return right;
+    k--;
+    if(k==0)
+        return root->data;
+    return kthSmallest(root->right, k);
+}

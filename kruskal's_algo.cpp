58c5da1e9738fd31729448d0d27a0c883b3299a1
Jatin <jt24072002@gmail.com>
2022-07-28 18:07:36 +0530

    kruskal's algo done

---
+++ kruskal's_algo.cpp
@@ -0,0 +1,34 @@
+#include<algorithm>
+bool comp(vector<int>&a,vector<int>&b){
+    return a[2]<b[2];
+}
+int findpar(int u,vector<int>&par){
+    if(u==par[u]) 
+        return u; 
+    return par[u]= findpar(par[u],par); 
+}
+
+int union_(int u, int v, vector<int> &par){
+    if(findpar(u,par)==findpar(v,par))  
+        return 0;
+    else if(!(u>=0 && v>=0 && u<par.size() && v<par.size())) 
+        return 0; 
+    par[u]=v; 
+}
+
+int kruskalMST(int n, int m, vector<vector<int>> &edges) {
+    sort(edges.begin(),edges.end(),comp); 
+    vector<int> parent(n+1);
+    for(int i=0;i<parent.size();i++) 
+        parent[i]=i;
+    
+    int cost=0;
+
+    for(auto it:edges){
+        if(findpar(it[0],parent)!=findpar(it[1],parent)){ 
+            cost+=it[2]; 
+            union_(findpar(it[0],parent),findpar(it[1],parent),parent); 
+        }
+    }
+    return cost;
+}
